e83a0340367df760b6391852fb8eb816f84b69f2
kTurek95 <k.turek1995@gmail.com>
2023-08-30 21:55:28 +0200

    adding few functions into module

---
+++ countries_tests.py
@@ -0,0 +1,53 @@
+import sqlite3
+
+from countries_function import get_info_from_db
+
+
+def create_database():
+    db_connection = sqlite3.connect('information.db')
+    cursor = db_connection.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Countries_and_capitals(
+        query_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        country VARCHAR,
+        capital VARCHAR,
+        population INTEGER
+    )''')
+
+    sample_data = [
+        ('Poland', 'Warsaw', 37950802),
+        ('Sweden', 'Stockholm', 10353442),
+        ('Portugal', 'Lisbon', 10305564),
+        ('France', 'Paris', 67391582),
+        ('Italy', 'Rome', 59554023)
+    ]
+
+    cursor.executemany('INSERT INTO Countries_and_capitals'
+                       '(country, capital, population)'
+                       'VALUES (?, ?, ?)', sample_data,)
+
+    db_connection.commit()
+    db_connection.close()
+
+
+def remove_database():
+    """
+    Removes the SQLite database file.
+    """
+    import os
+    os.remove('information.db')
+
+
+def test_get_info_from_db_test():
+    create_database()
+    db_connection = sqlite3.connect('information.db')
+    fetched_data = get_info_from_db(db_connection)
+    expected_returns = [
+        ('Poland', 37950802),
+        ('Sweden', 10353442),
+        ('Portugal', 10305564),
+        ('France', 67391582),
+        ('Italy', 59554023)
+    ]
+
+    remove_database()
+    assert fetched_data == expected_returns
